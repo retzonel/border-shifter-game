c6d5e47909a09c72a7ac93c1b657bece0947be60
retzonel <110589356+retzonel@users.noreply.github.com>
2026-05-03 14:47:04 +0100

    added movement and teleportation functionalities

--- Assets/_Project/Scripts/Player/TeleportController.cs
+++ Assets/_Project/Scripts/Player/TeleportController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,10 +15,9 @@ public class TeleportController : MonoBehaviour
 
     [SerializeField] private GameObject bluePorterPrefab, orangePorterPrefab;
 
-    [SerializeField]
-    private LineRenderer
+    [SerializeField] private LineRenderer
         teleportLineRenderer; //for later visual feedback of teleportation path from pplayer{blue portal) to mouse position(orange portal)
-    
+
     void OnEnable()
     {
         interactAction.action.Enable();
@@ -34,10 +34,18 @@ public class TeleportController : MonoBehaviour
 
     void Start()
     {
+        Physics2D.queriesHitTriggers = true;
         interactAction.action.started += ctx =>
         {
             //enter/toggle teleportation process
             AttemptingTeleport = !AttemptingTeleport;
+            PlacePortal(bluePorterPrefab, transform.position, ref currentBluePortal);
+            if (!AttemptingTeleport)
+            {
+                //if teleportation process is canceled, remove the blue portal
+                if (currentBluePortal != null)
+                    Destroy(currentBluePortal);
+            }
         };
         mouseClickAction.action.performed += ctx =>
         {
@@ -53,6 +61,11 @@ public class TeleportController : MonoBehaviour
         currentCharges = maxCharges;
     }
 
+    private void Update()
+    {
+        LineRendererSetup();
+    }
+
     private void TryTeleport()
     {
         if (currentCharges <= 0)
@@ -63,6 +76,10 @@ public class TeleportController : MonoBehaviour
         }
 
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(_pointerInput);
+        
+        
+        PlacePortal(orangePorterPrefab, worldPos, ref currentOrangePortal);
+        
         transform.position = worldPos;
         currentCharges--;
         AttemptingTeleport = false;
@@ -71,5 +88,32 @@ public class TeleportController : MonoBehaviour
 
         if (currentCharges <= 0)
             GameManager.Instance?.OnTeleportsExhausted();
+    }
+
+    void LineRendererSetup()
+    {
+        if (AttemptingTeleport && teleportLineRenderer != null)
+        {
+            // Show line from player to mouse position
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(_pointerInput);
+            teleportLineRenderer.enabled = true;
+            teleportLineRenderer.SetPosition(0, transform.position);
+            teleportLineRenderer.SetPosition(1, worldPos);
+        }
+        else if (teleportLineRenderer != null)
+        {
+            teleportLineRenderer.enabled = false;
+        }
+    }
+    
+    private GameObject currentBluePortal;
+    private GameObject currentOrangePortal;
+    private void PlacePortal(GameObject portalPrefab, Vector2 position, ref GameObject currentPortal)
+    {
+        if (currentPortal != null)
+            Destroy(currentPortal);
+    
+        if (portalPrefab != null)
+            currentPortal = Instantiate(portalPrefab, position, Quaternion.identity);
     }
 }
--- Assets/_Project/Scripts/Resource/ResourceNode.cs
+++ Assets/_Project/Scripts/Resource/ResourceNode.cs
@@ -1,9 +1,24 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class ResourceNode : MonoBehaviour
 {
     public ResourceData data;
     public bool isPickedUp = false;
+
+    private void Start()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        if (data != null && data.sprite != null) 
+        {
+            spriteRenderer.sprite = data.sprite;
+        }
+        else
+        {
+            Debug.LogWarning("nulref for " + gameObject.name);
+        }
+    }
 
     public void OnPickUp()
     {
